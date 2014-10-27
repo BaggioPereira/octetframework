877460a2ef651bdd054444d9482f62bc63eb6d8f
Baggio Pereira <lemonsjoke@users.sourceforge.net>
2014-10-27 17:27:32 +0000

    created tunnel for game area

--- octet/src/examples/octet_game/octet_game.h
+++ octet/src/examples/octet_game/octet_game.h
@@ -30,8 +30,43 @@ namespace octet
 		{
 			app_scene = new visual_scene();
 			app_scene->create_default_camera_and_lights();
+			app_scene->get_camera_instance(0)->get_node()->translate(vec3(0, 5, 20));
 			app_scene->get_camera_instance(0)->set_far_plane(20000);
 			app_scene->get_camera_instance(0)->set_near_plane(1);
+
+			scene_node *cam = app_scene->get_camera_instance(0)->get_node();
+			cam->translate(vec3(0, 0, 950));
+
+			material *groundMat = new material(vec4(1, 1, 0, 1));
+			material *wall = new material(vec4(1, 0, 0, 1));
+			material *sky = new material(vec4(0, 0, 128, 1));
+			material *playerMat = new material(vec4(1, 0, 1, 1));
+
+			mesh *groundMesh = new mesh_box(vec3(10, 0, 1000));
+			mesh *skyMesh = new mesh_box(vec3(25, 0, 1000));
+
+			scene_node *floorNode = new scene_node();
+			scene_node *leftWallNode = new scene_node();
+			scene_node *rightWallNode = new scene_node();
+			scene_node *skyNode = new scene_node();
+
+			app_scene->add_child(floorNode);
+			app_scene->add_child(leftWallNode);
+			app_scene->add_child(rightWallNode);
+			app_scene->add_child(skyNode);
+			
+			leftWallNode->translate(vec3(-15, 7.5, 0));
+			rightWallNode->translate(vec3(15, 7.5, 0));
+			leftWallNode->rotate(-45, vec3(0, 0, 1));
+			rightWallNode->rotate(45, vec3(0, 0, 1));
+			skyNode->translate(vec3(0, 15, 0));
+			
+
+			app_scene->add_mesh_instance(new mesh_instance(floorNode, groundMesh, groundMat));
+			app_scene->add_mesh_instance(new mesh_instance(leftWallNode, groundMesh, wall));
+			app_scene->add_mesh_instance(new mesh_instance(rightWallNode, groundMesh, wall));
+			app_scene->add_mesh_instance(new mesh_instance(skyNode, skyMesh, sky));
+
 		}
 
 		void draw_world(int x, int y, int w, int h)
@@ -39,6 +74,17 @@ namespace octet
 			int vx = 0, vy = 0;
 			get_viewport_size(vx, vy);
 			app_scene->begin_render(vx, vy);
+
+			scene_node *cam = app_scene->get_camera_instance(0)->get_node();
+
+			if (is_key_down(key_down))
+			{
+				cam->translate(vec3(0, 0, 1));
+			}
+			else if (is_key_down(key_up))
+			{
+				cam->translate(vec3(0, 0, -1));
+			}
 
 			app_scene->update(1.0f / 30);
 			app_scene->render((float)vx / vy);
