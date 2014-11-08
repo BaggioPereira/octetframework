c15f21025bac33451e9ce5f9839b0a980d663a14
Baggio Pereira <lemonsjoke@users.sourceforge.net>
2014-11-08 15:10:23 +0000

    added comments to octet_demo.h

--- octet/src/examples/octet_game/octet_demo.h
+++ octet/src/examples/octet_game/octet_demo.h
@@ -3,20 +3,21 @@ namespace octet
 	class octet_demo :public app
 	{
 		ref<visual_scene> app_scene;
-		btDefaultCollisionConfiguration config;
-		btCollisionDispatcher *dispatcher;
-		btDbvtBroadphase *broadphase;
-		btSequentialImpulseConstraintSolver *solver;
-		btDiscreteDynamicsWorld *world;
+		btDefaultCollisionConfiguration config; //setup for the world
+		btCollisionDispatcher *dispatcher; //handler for collisions between objects
+		btDbvtBroadphase *broadphase; //handler for rough collisions
+		btSequentialImpulseConstraintSolver *solver; //handler for resolve collisions
+		btDiscreteDynamicsWorld *world; //the physics world, with rigid bodies
 
 		mat4t worldCoord;
 
-		dynarray<btRigidBody*> rigid_bodies;
-		material *ballMat;
-		mesh_sphere *smallSphere;
-		mesh_sphere *sphere;
-		random r;
+		dynarray<btRigidBody*> rigid_bodies; //dynamic array for rigid bodies
+		material *ballMat; //material for the balls
+		mesh_sphere *smallSphere; //mesh for a small ball
+		mesh_sphere *sphere; //mesh for a large ball
+		random r; //random number generator
 
+		// function to add a mesh to the physics world
 		void add_shape(mat4t_in mat, mesh *msh, material *mtl, bool is_dynamic)
 		{
 			scene_node *node = new scene_node();
@@ -46,6 +47,7 @@ namespace octet
 			}
 		}
 
+		//function to add balls to the world on key input
 		void addBalls()
 		{
 			int x = 0, y = 0, z = 0;
@@ -68,6 +70,7 @@ namespace octet
 			worldCoord.loadIdentity();
 		}
 
+		//function to apply a force to the balls depending on key input
 		void applyForce()
 		{
 			if (is_key_going_down(key_up))
@@ -101,6 +104,7 @@ namespace octet
 		}
 
 	public:
+		//constructor for the class
 		octet_demo(int argc, char**argv) : app(argc, argv)
 		{
 			dispatcher = new btCollisionDispatcher(&config);
@@ -109,6 +113,7 @@ namespace octet
 			world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, &config);
 		}
 
+		//destructor for the class
 		~octet_demo()
 		{
 			delete world;
@@ -117,12 +122,14 @@ namespace octet
 			delete dispatcher;
 		}
 
+		//function that is called once OpenGL is initalized
 		void app_init()
 		{
-			r.set_seed(time(NULL));
-			app_scene = new visual_scene();
-			app_scene->create_default_camera_and_lights();
+			r.set_seed(time(NULL)); //sets the random seed
+			app_scene = new visual_scene(); //add a visual scene
+			app_scene->create_default_camera_and_lights(); //create default and camera for the scene
 			
+			//add a shader to for the material, creates transparency so you can see inside
 			image *transparentImg = new image("assets/transparent.jpg");
 			image *transparentmask = new image("assets/transparent.gif");
 			param_shader *shader = new param_shader("shaders/default.vs", "shaders/multitexture.fs");
@@ -130,11 +137,12 @@ namespace octet
 			transparent->add_sampler(0, app_utils::get_atom("transparentImg"), transparentImg, new sampler());
 			transparent->add_sampler(1, app_utils::get_atom("transparentmask"), transparentmask, new sampler());
 
-			mesh_box *cubeWall = new mesh_box(vec3(10.0f, 0.0f, 10.0f));
-			sphere = new mesh_sphere(vec3(0, 0, 0), 1.0f);
-			smallSphere = new mesh_sphere(vec3(0, 0, 0), 0.5f);
-			ballMat = new material(vec4(0, 0, 1, 1));
+			mesh_box *cubeWall = new mesh_box(vec3(10.0f, 0.0f, 10.0f)); //create a flat mesh box
+			sphere = new mesh_sphere(vec3(0, 0, 0), 1.0f); //set the size of the large ball
+			smallSphere = new mesh_sphere(vec3(0, 0, 0), 0.5f); //set the size of the small ball
+			ballMat = new material(vec4(0, 0, 1, 1)); //set the material of the ball
 
+			//creates a box with transparent material and adds friction and restitution
 			worldCoord.translate(vec3(0, -10, 0));
 			add_shape(worldCoord, cubeWall, transparent, false);
 			rigid_bodies.back()->setFriction(-0.5);
@@ -175,6 +183,7 @@ namespace octet
 			rigid_bodies.back()->setRestitution(1);
 			worldCoord.loadIdentity();
 
+			//commands printed out to console, instructions for the demo
 			printf("Press A to add a small ball\n");
 			printf("Press S to add a bigger ball\n");
 			printf("Press Up to add apply a upward force on the balls\n");
@@ -183,15 +192,17 @@ namespace octet
 			printf("Press Right to add apply a right force on the balls\n");
 		}
 
+		//function called to draw the world
 		void draw_world(int x, int y, int w, int h)
 		{
 			int vx = 0, vy = 0;
 			get_viewport_size(vx, vy);
 			app_scene->begin_render(vx, vy);
 			
-			addBalls();
-			applyForce();
+			addBalls(); //calls function to add balls
+			applyForce(); //calls function that adds force
 
+			//update for the collisions, at 30 frames per second
 			world->stepSimulation(1.0f / 30, 1.0f / 30, 1.0f / 30);
 			btCollisionObjectArray &colArray = world->getCollisionObjectArray();
 			for (unsigned i = 0; i != colArray.size(); ++i)
@@ -205,8 +216,10 @@ namespace octet
 				}
 			}
 
-			
+			//update matrices, at 30 frames per second
 			app_scene->update(1.0f / 30);
+
+			//redraw the scene
 			app_scene->render((float)vx / vy);
 		}
 	};
