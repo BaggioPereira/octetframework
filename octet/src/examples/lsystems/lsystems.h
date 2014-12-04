8cc48bc787fbee29c4b0ae06073eb1674ad64222
Baggio Pereira <lemonsjoke@users.sourceforge.net>
2014-12-04 17:17:22 +0000

    changes: loads certain parts of the file depending on which tree is selected, prints out the while tree statement

--- octet/src/examples/lsystems/lsystems.h
+++ octet/src/examples/lsystems/lsystems.h
@@ -76,10 +76,140 @@ namespace octet
 					{
 						axiom.push_back(read[i]);
 					}
+				}
+			}
 
-					else if (read[i] == ';')
+			if (treeNum == 2)
+			{
+				int startLoc = str.find("tree2");
+				printf("%d\n", startLoc);
+				if (startLoc == -1)
+				{
+					printf("Not found start of tree");
+				}
+
+				startLoc += 12;
+				int endLoc = str.find("};2");
+				if (endLoc == -1)
+				{
+					printf("Not found end of tree");
+				}
+
+				for (int i = startLoc; i < endLoc; ++i)
+				{
+					int axiomStr = str.find("axiom");
+					axiomStr += 5;
+					if (read[i] != ';')
 					{
-						break;
+						axiom.push_back(read[i]);
+					}
+				}
+			}
+
+			if (treeNum == 3)
+			{
+				int startLoc = str.find("tree3");
+				printf("%d\n", startLoc);
+				if (startLoc == -1)
+				{
+					printf("Not found start of tree");
+				}
+
+				startLoc += 12;
+				int endLoc = str.find("};3");
+				if (endLoc == -1)
+				{
+					printf("Not found end of tree");
+				}
+
+				for (int i = startLoc; i < endLoc; ++i)
+				{
+					int axiomStr = str.find("axiom");
+					axiomStr += 5;
+					if (read[i] != ';')
+					{
+						axiom.push_back(read[i]);
+					}
+				}
+			}
+
+			if (treeNum == 4)
+			{
+				int startLoc = str.find("tree4");
+				printf("%d\n", startLoc);
+				if (startLoc == -1)
+				{
+					printf("Not found start of tree");
+				}
+
+				startLoc += 12;
+				int endLoc = str.find("};4");
+				if (endLoc == -1)
+				{
+					printf("Not found end of tree");
+				}
+
+				for (int i = startLoc; i < endLoc; ++i)
+				{
+					int axiomStr = str.find("axiom");
+					axiomStr += 5;
+					if (read[i] != ';')
+					{
+						axiom.push_back(read[i]);
+					}
+				}
+			}
+
+			if (treeNum == 5)
+			{
+				int startLoc = str.find("tree5");
+				printf("%d\n", startLoc);
+				if (startLoc == -1)
+				{
+					printf("Not found start of tree");
+				}
+
+				startLoc += 12;
+				int endLoc = str.find("};5");
+				if (endLoc == -1)
+				{
+					printf("Not found end of tree");
+				}
+
+				for (int i = startLoc; i < endLoc; ++i)
+				{
+					int axiomStr = str.find("axiom");
+					axiomStr += 5;
+					if (read[i] != ';')
+					{
+						axiom.push_back(read[i]);
+					}
+				}
+			}
+
+			if (treeNum == 6)
+			{
+				int startLoc = str.find("tree6");
+				printf("%d\n", startLoc);
+				if (startLoc == -1)
+				{
+					printf("Not found start of tree");
+				}
+
+				startLoc += 12;
+				int endLoc = str.find("};6");
+				if (endLoc == -1)
+				{
+					printf("Not found end of tree");
+				}
+
+				for (int i = startLoc; i < endLoc; ++i)
+				{
+					int axiomStr = str.find("axiom");
+					axiomStr += 5;
+					if (read[i] != ';')
+					{
+						axiom.push_back(read[i]);
 					}
 				}
 			}
@@ -100,8 +230,6 @@ namespace octet
 			app_scene->create_default_camera_and_lights();
 			axiom.empty();
 			loadFile();
-			getVariables(1);
-			printf("%s\n", axiom.data());
 		}
 
 		void draw_world(int x, int y, int w, int h)
@@ -113,6 +241,48 @@ namespace octet
 			app_scene->update(1.0f / 30);
 
 			app_scene->render((float)vx / vy);
+
+			if (is_key_going_down('1') || is_key_going_down(VK_NUMPAD1))
+			{
+				getVariables(1);
+				printf("%s\n", axiom.data());
+				axiom.reset();
+			}
+
+			else if (is_key_going_down('2') || is_key_going_down(VK_NUMPAD2))
+			{
+				getVariables(2);
+				printf("%s\n", axiom.data());
+				axiom.reset();
+			}
+
+			else if (is_key_going_down('3') || is_key_going_down(VK_NUMPAD3))
+			{
+				getVariables(3);
+				printf("%s\n", axiom.data());
+				axiom.reset();
+			}
+
+			else if (is_key_going_down('4') || is_key_going_down(VK_NUMPAD4))
+			{
+				getVariables(4);
+				printf("%s\n", axiom.data());
+				axiom.reset();
+			}
+
+			else if (is_key_going_down('5') || is_key_going_down(VK_NUMPAD5))
+			{
+				getVariables(5);
+				printf("%s\n", axiom.data());
+				axiom.reset();
+			}
+
+			else if (is_key_going_down('6') || is_key_going_down(VK_NUMPAD6))
+			{
+				getVariables(6);
+				printf("%s\n", axiom.data());
+				axiom.reset();
+			}
 		}
 
 	};
