422065238c57d283d373b9ae348b29c13a2a07c2
Baggio Pereira <lemonsjoke@users.sourceforge.net>
2014-12-04 16:54:19 +0000

    trying methods for getting axiom, rules from txt file

--- octet/src/examples/lsystems/lsystems.h
+++ octet/src/examples/lsystems/lsystems.h
@@ -5,7 +5,91 @@ namespace octet
 	class lsystems : public app{
 		ref<visual_scene> app_scene;
 
+		dynarray<char> read;
+		dynarray<char> axiom;
+		hash_map<char, string> rules;
+
+		dynarray<char> rule;
+
+		float angle; 
+		int iteration;
+		int max_iters;
+
 	public:
+		void loadFile()
+		{
+			//Loading the txt file
+			std::fstream myfile;
+
+			//Open text file
+			myfile.open("lsystems.txt", std::ios::in);
+
+			//Check if file is opened
+			if (!myfile.is_open())
+			{
+				printf("File not opened/not found \n");
+			}
+			else
+			{
+				printf("File opened \n");
+			}
+
+			//Get size of text file
+			myfile.seekg(0, myfile.end);
+			int length = myfile.tellg();
+			printf("length is %d \n", length);
+			myfile.seekg(0, myfile.beg);
+
+			//Resize the array
+			read.resize(length);
+			myfile.read(read.data(), length);
+
+			//Print out what text file contains
+			//printf("Text file contains \n%s", read.data());
+		}
+
+		void getVariables(int treeNum)
+		{
+			string str(read.data(), read.size());
+			//printf("String contains \n%s", string.c_str());
+			if (treeNum == 1)
+			{
+				int startLoc = str.find("tree1");
+				printf("%d\n", startLoc);
+				if (startLoc == -1)
+				{
+					printf("Not found start of tree");
+				}
+
+				startLoc += 12;
+				int endLoc = str.find("};1");
+				if (endLoc == -1)
+				{
+					printf("Not found end of tree");
+				}
+
+				for (int i = startLoc; i < endLoc; ++i)
+				{
+					int axiomStr = str.find("axiom");
+					axiomStr += 5;
+					if (read[i] != ';')
+					{
+						axiom.push_back(read[i]);
+					}
+
+					else if (read[i] == ';')
+					{
+						break;
+					}
+				}
+			}
+		}
+
+		void rewrite()
+		{
+
+		}
+
 		lsystems(int argc, char **argv) : app(argc, argv)
 		{
 		}
@@ -14,27 +98,11 @@ namespace octet
 		{
 			app_scene = new visual_scene();
 			app_scene->create_default_camera_and_lights();
-			std::ifstream myfile ("lsystems.txt");
-			std::string line;
-			if (myfile.is_open())
-			{
-				printf("file opened \n");
-				while (std::getline(myfile, line))
-				{
-
-					printf("string is %s \n", line.c_str());
-				}
-				
-				myfile.close();
-				printf("file closed \n");
-			}
-			else
-			{
-				printf("Cannot open file \n");
-			}
+			axiom.empty();
+			loadFile();
+			getVariables(1);
+			printf("%s\n", axiom.data());
 		}
-
-
 
 		void draw_world(int x, int y, int w, int h)
 		{
@@ -45,7 +113,6 @@ namespace octet
 			app_scene->update(1.0f / 30);
 
 			app_scene->render((float)vx / vy);
-			
 		}
 
 	};
