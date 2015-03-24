319c075db2028427289376de7a6b65a66dba0124
Baggio Pereira <lemonsjoke@users.sourceforge.net>
2015-03-24 19:36:46 +0000

    water: added txt files for waves, added read from txt file and stores to memory

--- octet/src/examples/water/water.h
+++ octet/src/examples/water/water.h
@@ -4,14 +4,27 @@
 //
 // Modular Framework for OpenGLES2 rendering on multiple platforms.
 //
+
+#include <fstream>
+#include <sstream>
+
 namespace octet {
   /// Scene containing a box with octet.
   class water : public app {
     // scene for drawing box
     ref<visual_scene> app_scene;
-	mesh_builder waterMesh;
 	ref<mesh> mWater;
+	mesh_builder waterMesh;
 	scene_node *cam;
+
+	dynarray<char> read;
+	dynarray<char> waveLength;
+	dynarray<char> amplitude;
+	dynarray<char> speed;
+	dynarray<char> steep;
+
+	int num = 1;
+
 	bool wire = false;
 
 	enum res {
@@ -23,6 +36,40 @@ namespace octet {
 
 	float H[WIDTH][HEIGHT], U[WIDTH][HEIGHT], V[WIDTH][HEIGHT];
 
+  public:
+    /// this is called when we construct the class before everything is initialised.
+    water(int argc, char **argv) : app(argc, argv) {
+	}
+
+	void loadWave(int waveNum)
+	{
+		std::fstream myFile;
+		std::stringstream waveStr;
+		std::string waveFile;
+		read.reset();
+		waveStr.clear();
+		waveFile.clear();
+		waveStr << "wave" << num <<".txt";
+		waveFile = waveStr.str();
+		myFile.open(waveFile, std::ios::in);
+
+		if (!myFile.is_open())
+		{
+			printf("File is not found/opened\n");
+		}
+		else
+		{
+			printf("File found/opened\n");
+		}
+		myFile.seekg(0, myFile.end);
+		int length = myFile.tellg();
+		myFile.seekg(0, myFile.beg);
+
+		read.resize(length);
+		myFile.read(read.data(), length);
+		myFile.close();
+	}
+	
 	void meshGeneration()
 	{
 		vec4 vertices[4];
@@ -32,10 +79,10 @@ namespace octet {
 		{
 			for (int x = 0; x < 40; x++)
 			{
-				vertices[0] = vec4(x*1.0f, H[x][z], z*1.0f, 1);
-				vertices[1] = vec4(x*1.0 + 1.0f, H[x + 1][z], z*1.0f, 1);
-				vertices[2] = vec4(x*1.0f + 1.0f, H[x + 1][z + 1], z*1.0f + 1.0f, 1);
-				vertices[3] = vec4(x*1.0f, H[x][z + 1], z*1.0f + 1.0f, 1);
+				vertices[0] = vec4(x*1.0f, 0.0f, z*1.0f, 1);
+				vertices[1] = vec4(x*1.0 + 1.0f, 0.0f, z*1.0f, 1);
+				vertices[2] = vec4(x*1.0f + 1.0f, 0.0f, z*1.0f + 1.0f, 1);
+				vertices[3] = vec4(x*1.0f, 0.0f, z*1.0f + 1.0f, 1);
 
 				for (size_t i = 0; i < 4; ++i) {
 					waterMesh.add_vertex(vertices[i], vec4(1, 1, 1, 1), 1, 1);
@@ -53,29 +100,12 @@ namespace octet {
 		}
 	}
 
-	void setup()
-	{
-		for (int i = 0; i < PADDEDWIDTH; i++){
-			for (int j = 0; j < PADDEDHEIGHT; j++){
-				H[i][j] = 1.0f;
-				U[i][j] = 0.0f;
-				V[i][j] = 0.0f;
-			}
-		}
-	}
-
-  public:
-    /// this is called when we construct the class before everything is initialised.
-    water(int argc, char **argv) : app(argc, argv) {
-	}
-
     /// this is called once OpenGL is initialized
     void app_init() {
       app_scene =  new visual_scene();
       app_scene->create_default_camera_and_lights();
 	  scene_node *water_node = app_scene->add_scene_node();
 	  material *blue = new material(vec4(0, 0, 1, 1));
-	  setup();
 	  waterMesh.init();
 	  meshGeneration();
 	  mWater = new mesh();
@@ -84,6 +114,7 @@ namespace octet {
 	  app_scene->add_mesh_instance(new mesh_instance(water_node, mWater, blue));
 	  cam = app_scene->get_camera_instance(0)->get_node();
 	  cam->translate(vec3(20, 10, 40));
+	  loadWave(num);
     }
 
     /// this is called to draw the world
@@ -108,6 +139,25 @@ namespace octet {
 			  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 			  wire = true;
 		  }
+	  }
+
+	  if (is_key_going_down(key_left))
+	  {
+		  if (num > 1)
+		  {
+			  num--;
+			  loadWave(num);
+		  }
+		  else
+		  {
+			  printf("No more waves\n");
+		  }
+	  }
+
+	  if (is_key_going_down(key_right))
+	  {
+		  num++;
+		  loadWave(num);
 	  }
 
       // update matrices. assume 30 fps.
