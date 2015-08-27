cf843c1a3a2f507e8d7922381513ba0fa3d4c50b
LemonsJoke <baggio_111111@hotmail.com>
2015-08-27 13:21:32 +0100

    Alternate method, doesnt use dynarrays

--- octet/src/examples/l_systems/l_systems.h
+++ octet/src/examples/l_systems/l_systems.h
@@ -14,30 +14,200 @@ namespace octet
 	class l_systems : public app 
 	{
 		//dynamic arrays
-		dynarray<char> readfile;
-		dynarray<char> axiom;
-		dynarray<char> alphabet;
-		dynarray<float> angle;
-		dynarray<int> interations;
+		//dynarray<char> axiom;
+		//dynarray<char> alphabet;
+		//dynarray<float> angle;
+		//dynarray<int> interations;
+
+		std::stringstream file;
+		string contents;
+		std::string alphabet, axiom, angle, iteration;
+
+		float angles;
+		int iterations;
 
 		hash_map<char, string> rules;
 
 		// scene for drawing box
 		ref<visual_scene> app_scene;
 
-		char treenum[64];
-		std::stringstream tree;
+		int defaultTree = 1;
+		int prevTree;
+		std::stringstream treeString;
 
 		//Tweakbar
 		TwBar *myBar;
+
 	public:
 		/// this is called when we construct the class before everything is initialised.
 		l_systems(int argc, char **argv) : app(argc, argv) {
 		}
 
-		void loadfile()
+		void fileLoading()
 		{
+			std::fstream myFile;
+			std::stringstream fileName;
+			fileName << "Tree " << defaultTree << ".txt";
+			myFile.open(fileName.str().c_str(), std::ios::in);
+			if (!myFile.is_open())
+			{
+				printf("File not opened/not found \n");
+			}
 
+			else
+			{
+				printf("File is open/found\n");
+				myFile.seekg(0, myFile.end);
+				int length = myFile.tellg();
+				myFile.seekg(0, myFile.beg);
+
+				file.str(std::string());
+				file << myFile.rdbuf();
+				myFile.close();
+
+				contents = file.str().c_str();
+
+				//printf("%s\n", contents.c_str());
+
+				getAlphabet();
+				getAxiom();
+				getAngle();
+				getIteration();
+				printf("\n");
+			}
+		}
+
+		void getAlphabet()
+		{
+			alphabet.clear();
+			int startLocation = contents.find("alphabet");
+			startLocation += 8;
+			int endLocation = contents.find("axiom");
+			endLocation -= 1;
+
+			for (int i = startLocation; i < endLocation; i++)
+			{
+				alphabet.push_back(contents[i]);
+			}
+
+			printf("Alphabets are %s\n", alphabet.c_str());
+		}
+
+		void getAxiom()
+		{
+			axiom.clear();
+			int startLocation = contents.find("axiom");
+			startLocation += 5;
+			int endLocation = contents.find("angle");
+			endLocation -= 1;
+
+			for (int i = startLocation; i < endLocation; i++)
+			{
+				axiom.push_back(contents[i]);
+			}
+
+			printf("axiom is %s\n", axiom.c_str());
+		}
+
+		void getAngle()
+		{
+			angle.clear();
+			int startLocation = contents.find("angle");
+			startLocation += 5;
+			int endLocation = contents.find("rule1");
+			endLocation -= 1;
+
+			for (int i = startLocation; i < endLocation; i++)
+			{
+				angle.push_back(contents[i]);
+			}
+
+			angles = atof(angle.c_str());
+			printf("Angle is %g\n", angles);
+		}
+
+		void getIteration()
+		{
+			iteration.clear();
+			int startLocation = contents.find("iterations");
+			startLocation += 10;
+			int endLocation = contents.find(";");
+
+			for (int i = startLocation; i < endLocation; i++)
+			{
+				iteration.push_back(contents[i]);
+			}
+
+			iterations = atoi(iteration.c_str());
+			printf("Iteration is %d\n", iterations);
+		}
+
+		//Mouse input
+		void mouseInputControl()
+		{
+			int mX = 0, mY = 0;
+			get_mouse_pos(mX, mY);
+			TwMouseMotion(mX, mY);
+			if (is_key_going_down(key_lmb))
+			{
+				TwMouseButton(TW_MOUSE_PRESSED, TW_MOUSE_LEFT);
+			}
+			if (!is_key_down(key_lmb) && get_prev_keys()[key_lmb] != 0)
+			{
+				TwMouseButton(TW_MOUSE_RELEASED, TW_MOUSE_LEFT);
+			}
+		}
+
+		//key input
+		void keyboardTranslation()
+		{
+			//Numbers
+			if (is_key_going_down('1') || is_key_going_down(VK_NUMPAD1))
+			{
+				TwKeyPressed('1', 0);
+			}
+			if (is_key_going_down('2') || is_key_going_down(VK_NUMPAD2))
+			{
+				TwKeyPressed('2', 0);
+			}
+			if (is_key_going_down('3') || is_key_going_down(VK_NUMPAD3))
+			{
+				TwKeyPressed('3', 0);
+			}
+			if (is_key_going_down('4') || is_key_going_down(VK_NUMPAD4))
+			{
+				TwKeyPressed('4', 0);
+			}
+			if (is_key_going_down('5') || is_key_going_down(VK_NUMPAD5))
+			{
+				TwKeyPressed('5', 0);
+			}
+			if (is_key_going_down('6') || is_key_going_down(VK_NUMPAD6))
+			{
+				TwKeyPressed('6', 0);
+			}
+			if (is_key_going_down('7') || is_key_going_down(VK_NUMPAD7))
+			{
+				TwKeyPressed('7', 0);
+			}
+			if (is_key_going_down('8') || is_key_going_down(VK_NUMPAD8))
+			{
+				TwKeyPressed('8', 0);
+			}
+			if (is_key_going_down('9') || is_key_going_down(VK_NUMPAD9))
+			{
+				TwKeyPressed('9', 0);
+			}
+			if (is_key_going_down('0') || is_key_going_down(VK_NUMPAD0))
+			{
+				TwKeyPressed('0', 0);
+			}
+			//control values using ascii found here
+			//http://www.asciitable.com/
+			if (is_key_going_down(key_backspace))
+			{
+				TwKeyPressed(8, 0);
+			}
 		}
 
 		/// this is called once OpenGL is initialized
@@ -46,8 +216,9 @@ namespace octet
 			app_scene =  new visual_scene();
 			app_scene->create_default_camera_and_lights();
 			TwInit(TW_OPENGL, NULL);
-			TwWindowSize(100, 100);
+			TwWindowSize(750, 720);
 			myBar = TwNewBar("L Systems");
+			TwAddVarRW(myBar, "Tree Selected", TW_TYPE_INT32, &defaultTree, "help ='Type in the tree you would like to load'");
 		}
 
 		/// this is called to draw the world
@@ -63,7 +234,15 @@ namespace octet
 			// draw the scene
 			app_scene->render((float)vx / vy);
 			TwDraw();
-		}
+			mouseInputControl();
+			keyboardTranslation();
 
+			if (prevTree != defaultTree)
+			{
+				printf("Tree selected %d\n", defaultTree);
+				prevTree = defaultTree;
+				fileLoading();
+			}
+		}
 	};
 }
